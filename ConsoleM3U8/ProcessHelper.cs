c84de77f74a853571e3aee02604ca02c254171b9
bebestmaple <447349864@qq.com>
2024-02-23 06:41:12 +0000

    1

---
+++ ConsoleM3U8/ProcessHelper.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+
+namespace ConsoleM3U8;
+public class ProcessHelper
+{
+    public static void Excute(string fileNamePath, string arguments = "")
+    {
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = fileNamePath,
+            Arguments = arguments, 
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        var process = new Process
+        {
+            StartInfo = startInfo
+        };
+
+        process.OutputDataReceived += Process_OutputDataReceived;
+        process.ErrorDataReceived += Process_ErrorDataReceived;
+
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+    }
+
+    private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Data))
+        {
+            Console.WriteLine(e.Data); 
+        }
+    }
+
+    private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Data))
+        {
+            Console.WriteLine($"Error: {e.Data}");
+        }
+    }
+}
